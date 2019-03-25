d3597c9443b97b33f36cd7a838693e644cb63c36
felix choi <felix.choi@free.fr>
2019-03-25 12:38:39 +0100

    versiopn 1 nul debug

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -14,6 +14,303 @@
 #include "pathtracer.h"
 #include <mpi.h>
 
+
+
+/********** micro BLAS LEVEL-1 + quelques fonctions non-standard **************/
+static inline void copy(const double *x, double *y) // copy x dans y. x et y etant des vecteurs de dim3
+{
+	for (int i = 0; i < 3; i++)
+		y[i] = x[i];
+} 
+
+static inline void zero(double *x) //x = 0. x vecteur 3D
+{
+	for (int i = 0; i < 3; i++)
+		x[i] = 0;
+} 
+
+static inline void axpy(double alpha, const double *x, double *y) // y = y+ ax
+{
+	for (int i = 0; i < 3; i++)
+		y[i] += alpha * x[i];
+} 
+
+static inline void scal(double alpha, double *x) // x = ax
+{
+	for (int i = 0; i < 3; i++)
+		x[i] *= alpha;
+} 
+
+static inline double dot(const double *a, const double *b) //multiplication de vecteurs
+{ 
+	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+} 
+
+static inline double nrm2(const double *a) // norme du vecteur
+{
+	return sqrt(dot(a, a));
+}
+
+/********* fonction non-standard *************/
+static inline void mul(const double *x, const double *y, double *z)// multiplication element par element
+{
+	for (int i = 0; i < 3; i++)
+		z[i] = x[i] * y[i];
+} 
+
+static inline void normalize(double *x) // normalisation de X
+{
+	scal(1 / nrm2(x), x);
+}
+
+/* produit vectoriel */
+static inline void cross(const double *a, const double *b, double *c)
+{
+	c[0] = a[1] * b[2] - a[2] * b[1];
+	c[1] = a[2] * b[0] - a[0] * b[2];
+	c[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+/****** tronque *************/
+static inline void clamp(double *x) // encadrement des valeurs des elements de X entre 0 et 1.
+{
+	for (int i = 0; i < 3; i++) {
+		if (x[i] < 0)
+			x[i] = 0;
+		if (x[i] > 1)
+			x[i] = 1;
+	}
+} 
+
+/******************************* calcul des intersections rayon / sphere *************************************/
+   
+// returns distance, 0 if nohit 
+double sphere_intersect(const struct Sphere *s, const double *ray_origin, const double *ray_direction) // renvoie la distance entre l'origine de rayon et le point d'intersection avec la sphere
+																									   //nous permettant de connaitre le point d'intersection (point ray_origine + distance + ray_direction)
+{ 
+	double op[3];
+	// Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0 
+	copy(s->position, op);    // op devient la position de la sphere
+	axpy(-1, ray_origin, op); //op est modifié, opn devient la distance entre s et ray origine
+	double eps = 1e-4;
+	double b = dot(op, ray_direction);
+	double discriminant = b * b - dot(op, op) + s->radius * s->radius; 
+	if (discriminant < 0)
+		return 0;   /* pas d'intersection */
+	else 
+		discriminant = sqrt(discriminant);
+	/* détermine la plus petite solution positive (i.e. point d'intersection le plus proche, mais devant nous) */
+	double t = b - discriminant;
+	if (t > eps) {
+		return t;
+	} else {
+		t = b + discriminant;
+		if (t > eps)
+			return t;
+		else
+			return 0;  /* cas bizarre, racine double, etc. */
+	}
+}
+
+/* détermine si le rayon intersecte l'une des spere; si oui renvoie true et fixe t, id */
+bool intersect(const double *ray_origin, const double *ray_direction, double *t, int *id) //entre autre cherche la premiere boule 
+{ 
+	int n = sizeof(spheres) / sizeof(struct Sphere);
+	double inf = 1e20; 
+	*t = inf;
+	for (int i = 0; i < n; i++) {
+		double d = sphere_intersect(&spheres[i], ray_origin, ray_direction);
+		if ((d > 0) && (d < *t)) {
+			*t = d;
+			*id = i;
+		} 
+	}
+	return *t < inf;
+} 
+
+/* calcule (dans out) la lumiance reçue par la camera sur le rayon donné */
+void radiance(const double *ray_origin, const double *ray_direction, int depth, unsigned short *PRNG_state, double *out)
+{ 
+	int id = 0;                             // id de la sphère intersectée par le rayon
+	double t;                               // distance à l'intersection
+	if (!intersect(ray_origin, ray_direction, &t, &id)) {
+		zero(out);    // if miss, return black 
+		return; 
+	}
+	const struct Sphere *obj = &spheres[id];
+	
+	/* point d'intersection du rayon et de la sphère */
+	double x[3];
+	copy(ray_origin, x);
+	axpy(t, ray_direction, x);
+	
+	/* vecteur normal à la sphere, au point d'intersection */
+	double n[3];  
+	copy(x, n);
+	axpy(-1, obj->position, n);
+	normalize(n);
+	
+	/* vecteur normal, orienté dans le sens opposé au rayon 
+	   (vers l'extérieur si le rayon entre, vers l'intérieur s'il sort) */
+	double nl[3];
+	copy(n, nl);
+	if (dot(n, ray_direction) > 0)
+		scal(-1, nl);
+	
+	/* couleur de la sphere */
+	double f[3];
+	copy(obj->color, f);
+	double p = obj->max_reflexivity;
+
+	/* processus aléatoire : au-delà d'une certaine profondeur,
+	   décide aléatoirement d'arrêter la récusion. Plus l'objet est
+	   clair, plus le processus a de chance de continuer. */
+	depth++;
+	if (depth > KILL_DEPTH) {
+		if (erand48(PRNG_state) < p) {
+			scal(1 / p, f); 
+		} else {
+			copy(obj->emission, out);
+			return;
+		}
+	}
+
+	/* Cas de la réflection DIFFuse (= non-brillante). 
+	   On récupère la luminance en provenance de l'ensemble de l'univers. 
+	   Pour cela : (processus de monte-carlo) on choisit une direction
+	   aléatoire dans un certain cone, et on récupère la luminance en 
+	   provenance de cette direction. */
+	if (obj->refl == DIFF) {
+		double r1 = 2 * M_PI * erand48(PRNG_state);  /* angle aléatoire */
+		double r2 = erand48(PRNG_state);             /* distance au centre aléatoire */
+		double r2s = sqrt(r2); 
+		
+		double w[3];   /* vecteur normal */
+		copy(nl, w);
+		
+		double u[3];   /* u est orthogonal à w */
+		double uw[3] = {0, 0, 0};
+		if (fabs(w[0]) > .1)
+			uw[1] = 1;
+		else
+			uw[0] = 1;
+		cross(uw, w, u);
+		normalize(u);
+		
+		double v[3];   /* v est orthogonal à u et w */
+		cross(w, u, v);
+		
+		double d[3];   /* d est le vecteur incident aléatoire, selon la bonne distribution */
+		zero(d);
+		axpy(cos(r1) * r2s, u, d);
+		axpy(sin(r1) * r2s, v, d);
+		axpy(sqrt(1 - r2), w, d);
+		normalize(d);
+		
+		/* calcule récursivement la luminance du rayon incident */
+		double rec[3];
+		radiance(x, d, depth, PRNG_state, rec);
+		
+		/* pondère par la couleur de la sphère, prend en compte l'emissivité */
+		mul(f, rec, out);
+		axpy(1, obj->emission, out);
+		return;
+	}
+
+	/* dans les deux autres cas (réflection parfaite / refraction), on considère le rayon
+	   réfléchi par la spère */
+
+	double reflected_dir[3];
+	copy(ray_direction, reflected_dir);
+	axpy(-2 * dot(n, ray_direction), n, reflected_dir);
+
+	/* cas de la reflection SPEculaire parfaire (==mirroir) */
+	if (obj->refl == SPEC) { 
+		double rec[3];
+		/* calcule récursivement la luminance du rayon réflechi */
+		radiance(x, reflected_dir, depth, PRNG_state, rec);
+		/* pondère par la couleur de la sphère, prend en compte l'emissivité */
+		mul(f, rec, out);
+		axpy(1, obj->emission, out);
+		return;
+	}
+
+	/* cas des surfaces diélectriques (==verre). Combinaison de réflection et de réfraction. */
+	bool into = dot(n, nl) > 0;      /* vient-il de l'extérieur ? */
+	double nc = 1;                   /* indice de réfraction de l'air */
+	double nt = 1.5;                 /* indice de réfraction du verre */
+	double nnt = into ? (nc / nt) : (nt / nc);
+	double ddn = dot(ray_direction, nl);
+	
+	/* si le rayon essaye de sortir de l'objet en verre avec un angle incident trop faible,
+	   il rebondit entièrement */
+	double cos2t = 1 - nnt * nnt * (1 - ddn * ddn);
+	if (cos2t < 0) {
+		double rec[3];
+		/* calcule seulement le rayon réfléchi */
+		radiance(x, reflected_dir, depth, PRNG_state, rec);
+		mul(f, rec, out);
+		axpy(1, obj->emission, out);
+		return;
+	}
+	
+	/* calcule la direction du rayon réfracté */
+	double tdir[3];
+	zero(tdir);
+	axpy(nnt, ray_direction, tdir);
+	axpy(-(into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)), n, tdir);
+
+	/* calcul de la réflectance (==fraction de la lumière réfléchie) */
+	double a = nt - nc;
+	double b = nt + nc;
+	double R0 = a * a / (b * b);
+	double c = 1 - (into ? -ddn : dot(tdir, n));
+	double Re = R0 + (1 - R0) * c * c * c * c * c;   /* réflectance */
+	double Tr = 1 - Re;                              /* transmittance */
+	
+	/* au-dela d'une certaine profondeur, on choisit aléatoirement si
+	   on calcule le rayon réfléchi ou bien le rayon réfracté. En dessous du
+	   seuil, on calcule les deux. */
+	double rec[3];
+	if (depth > SPLIT_DEPTH) {
+		double P = .25 + .5 * Re;             /* probabilité de réflection */
+		if (erand48(PRNG_state) < P) {
+			radiance(x, reflected_dir, depth, PRNG_state, rec);
+			double RP = Re / P;
+			scal(RP, rec);
+		} else {
+			radiance(x, tdir, depth, PRNG_state, rec);
+			double TP = Tr / (1 - P); 
+			scal(TP, rec);
+		}
+	} else {
+		double rec_re[3], rec_tr[3];
+		radiance(x, reflected_dir, depth, PRNG_state, rec_re);
+		radiance(x, tdir, depth, PRNG_state, rec_tr);
+		zero(rec);
+		axpy(Re, rec_re, rec);
+		axpy(Tr, rec_tr, rec);
+	}
+	/* pondère, prend en compte la luminance */
+	mul(f, rec, out);
+	axpy(1, obj->emission, out);
+	return;
+}
+
+double wtime()
+{
+	struct timeval ts;
+	gettimeofday(&ts, NULL);
+	return (double)ts.tv_sec + ts.tv_usec / 1E6;
+}
+
+int toInt(double x)
+{
+	return pow(x, 1 / 2.2) * 255 + .5;   /* gamma correction = 2.2 */
+} 
+
+
+
 double my_gettimeofday(){
   struct timeval tmp_time;
   gettimeofday(&tmp_time, NULL);
