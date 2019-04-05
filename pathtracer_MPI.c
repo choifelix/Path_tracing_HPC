b60d6baa2f04b4ed992f5794464ba7817dd82a9e
felix <felix.choi@free.fr>
2019-04-05 16:43:14 +0200

    dynamic test

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -892,13 +892,15 @@ void version2_beta_dynamic(int argc, char **argv){
 			//MPI_Ibcast(line_number, 1, MPI_INT, k, MPI_COMM_WORLD,&req);
 			if(k !=rank){
 				MPI_Send(line_number,1,MPI_INT,k,0,MPI_COMM_WORLD);
-				MPI_Irecv(line,1,MPI_INT,k,0,MPI_COMM_WORLD,&req);
-				if(line> line_number){
-					line_number = line;
-				}
+				
 				//MPI_Bcast(line_number, 1, MPI_INT, k, MPI_COMM_WORLD);
 				//MPI_Ibcast(line_number, 1, MPI_INT, k, MPI_COMM_WORLD,&req);
 			}
+		}
+
+		MPI_Irecv(line,1,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD,&req);
+		if(line> line_number){
+			line_number = line;
 		}
 
 
