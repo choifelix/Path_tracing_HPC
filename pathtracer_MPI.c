0f92f0b76b85bfaa76967ffcde596a9d6c41e0c7
felix <felix.choi@free.fr>
2019-04-05 16:58:47 +0200

    dynamic test

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -888,6 +888,7 @@ void version2_beta_dynamic(int argc, char **argv){
 		if(*line> line_number){
 			line_number = *line;
 		}
+		printf("proc %d afetr Irecv\n",rank );
 
 		//MPI_Bcast(line_number, 1, MPI_INT, rank, MPI_COMM_WORLD);
 		i = line_number;
