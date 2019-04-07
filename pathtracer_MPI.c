b82755834061e52b1d191a04b2b03f8898b70ba7
felix <felix.choi@free.fr>
2019-04-07 23:01:13 +0200

    try to fix the bug of exe non terminated by prcessus 0

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -850,24 +850,24 @@ void version2_dynamic(int argc, char **argv){
 
 
 	if (rank == 0){
-		MPI_Request final_req;
-		int final_flag;
-		while(count_line <= h){
-			MPI_Irecv(tab,3*w+1,MPI_DOUBLE,MPI_ANY_SOURCE,1,MPI_COMM_WORLD,&final_req);
+		// MPI_Request final_req;
+		// int final_flag;
+		// while(count_line <= h){
+		// 	MPI_Irecv(tab,3*w+1,MPI_DOUBLE,MPI_ANY_SOURCE,1,MPI_COMM_WORLD,&final_req);
 		    
-			MPI_Test(&final_req,&final_flag,&status_tab);
-			if(final_flag){
-				//printf("%d recieve tab from %d \n",rank,status_tab.MPI_SOURCE);
-				line = tab[0];
+		// 	MPI_Test(&final_req,&final_flag,&status_tab);
+		// 	if(final_flag){
+		// 		//printf("%d recieve tab from %d \n",rank,status_tab.MPI_SOURCE);
+		// 		line = tab[0];
 
-		       	for(int k=1; k< 3*w+1; k++){
-		       		image[(line*3*w + k -1) ] = tab[k]; 
-		       	}
-		       	count_line++;
-		       	printf("nb line done : %d \n",count_line);
-		       	//image_map[line] = 1;
-			}
-		}
+		//        	for(int k=1; k< 3*w+1; k++){
+		//        		image[(line*3*w + k -1) ] = tab[k]; 
+		//        	}
+		//        	count_line++;
+		//        	printf("nb line done : %d \n",count_line);
+		//        	//image_map[line] = 1;
+		// 	}
+		// }
 
 
 		// for(int k=0 ; k<size ; k++){
