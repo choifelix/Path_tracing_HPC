bda4b88676e1da314c62acbe3a8bb97900b1676a
felix <felix.choi@free.fr>
2019-04-09 23:17:54 +0200

    anneau dynamic fiest test

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -1091,6 +1091,7 @@ void version2_beta_dynamic(int argc, char **argv){
 			else{
 				if(state = actif)
 					state = inactif;
+				MPI_wait(&req, &status);
 
 			}
 		}
@@ -1163,42 +1164,12 @@ void version2_beta_dynamic(int argc, char **argv){
 			}
 		
 
-			tab[0] = (double)i;
+			
 
 
-			if (rank == 0){
-
-		       	// printf(" line done : %d \n",line);
-		       	count_line++;
-		       	printf("done by %d nb line done : %d, line %d \n",rank,count_line, i);
-
-
-		       	for(int k=0 ; k<size-1 ; k++){
-
-		       		if(iter > 0 && flag_tab[k] == 0){
-						MPI_Request_free(&req_tab[k]);
-					}
-
-		       		MPI_Irecv(tab,3*w+1,MPI_DOUBLE,k+1,1,MPI_COMM_WORLD,&req_tab[k]);
-			    
-					MPI_Test(&req_tab[k],&flag_tab[k],&status_tab);
-					if(flag_tab[k]){
-						
-						line = tab[0];
-						//printf("%d recieve tab from %d with line %d \n",rank,status_tab.MPI_SOURCE, line);
-
-				       	for(int l=1; l< 3*w+1; l++){
-				       		image[(line*3*w + l -1) ] = tab[l]; 
-				       	}
-				       	count_line++;
-				       	printf("done by %d nb line done : %d, line %d \n",status_tab.MPI_SOURCE,count_line, line);
-					}
-				}
-
-			}
-			else{
-
-
+			
+			if(rank != 0){
+				tab[0] = (double)i;
 
 				for(int k=1 ; k<3*w+1 ; k++){
 					tab[k] = image[k-1];
@@ -1207,6 +1178,35 @@ void version2_beta_dynamic(int argc, char **argv){
 				// MPI_Isend(tab,3*w+1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&tmp_reg);
 				MPI_Send(tab,3*w+1,MPI_DOUBLE,0,1,MPI_COMM_WORLD);
 				
+			}
+		}
+		if (rank == 0){
+
+	       	// printf(" line done : %d \n",line);
+	       	count_line++;
+	       	printf("done by %d nb line done : %d, line %d \n",rank,count_line, i);
+
+
+	       	for(int k=0 ; k<size-1 ; k++){
+
+	       		if(iter > 0 && flag_tab[k] == 0){
+					MPI_Request_free(&req_tab[k]);
+				}
+
+	       		MPI_Irecv(tab,3*w+1,MPI_DOUBLE,k+1,1,MPI_COMM_WORLD,&req_tab[k]);
+		    
+				MPI_Test(&req_tab[k],&flag_tab[k],&status_tab);
+				if(flag_tab[k]){
+					
+					line = tab[0];
+					//printf("%d recieve tab from %d with line %d \n",rank,status_tab.MPI_SOURCE, line);
+
+			       	for(int l=1; l< 3*w+1; l++){
+			       		image[(line*3*w + l -1) ] = tab[l]; 
+			       	}
+			       	count_line++;
+			       	printf("done by %d nb line done : %d, line %d \n",status_tab.MPI_SOURCE,count_line, line);
+				}
 			}
 		}
 		iter++;
