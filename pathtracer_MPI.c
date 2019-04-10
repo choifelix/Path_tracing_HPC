20d1572a55228712c1c4c6fc6fca59c550e3220c
felix <felix.choi@free.fr>
2019-04-10 11:26:19 +0200

    anneau dynamic fiest test

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -1323,19 +1323,21 @@ void version2_beta_dynamic(int argc, char **argv){
 	       	for(int k=0 ; k<size-1 ; k++){
 	       		MPI_Test(&req_tab[k],&flag_tab[k],&status_tab);
 
-	       		if(iter > 0 && flag_tab[k] == 0){
-					MPI_Request_free(&req_tab[k]);
-				}
-				else{
-					line = tab[0];
-					//printf("%d recieve tab from %d with line %d \n",rank,status_tab.MPI_SOURCE, line);
+	       		if(iter > 0){
+	       			if(flag_tab[k])
+						line = tab[0];
+						//printf("%d recieve tab from %d with line %d \n",rank,status_tab.MPI_SOURCE, line);
 
-			       	for(int l=1; l< 3*w+1; l++){
-			       		image[(line*3*w + l -1) ] = tab[l]; 
-			       	}
-			       	count_line++;
-			       	printf("done by %d nb line done : %d, line %d \n",status_tab.MPI_SOURCE,count_line, line);
-				}
+				       	for(int l=1; l< 3*w+1; l++){
+				       		image[(line*3*w + l -1) ] = tab[l]; 
+				       	}
+				       	count_line++;
+				       	printf("done by %d nb line done : %d, line %d \n",status_tab.MPI_SOURCE,count_line, line);
+				
+					else{
+						
+				       	MPI_Request_free(&req_tab[k]);
+					}
 
 	       		MPI_Irecv(tab,3*w+1,MPI_DOUBLE,k+1,1,MPI_COMM_WORLD,&req_tab[k]);
 		    
