8a42a57a00c385a9ccd70b48e5fb87510a483850
felix choi <felix.choi@free.fr>
2019-04-24 01:09:40 +0200

    trying fix seg faukt

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -1733,13 +1733,13 @@ void traitement_token(int rank, int size,int token, bool work, int *state, bool 
 	else if(token >= 0){
 		printf("proc %d  case : >=0\n",rank);
 		if(work){
-			int line_left = work_limit - *i;
-			*i++;
+			int line_left = work_limit - i;
+			i++;
 			// int token_t = *i;
 			// int *token_tmp;
 			// token_tmp = &token_t;
 			// MPI_Send(token_tmp,1,MPI_INT,token,0,MPI_COMM_WORLD);
-			*i++;
+			i++;
 		}
 		else{
 			if(token == rank){
