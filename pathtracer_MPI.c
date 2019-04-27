e91e64be0c3c2dcedf57e44661af47ef72ad4923
felix choi <felix.choi@free.fr>
2019-04-27 11:36:37 +0200

    version4 openMP, changed image name

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -2356,7 +2356,7 @@ void version4_openMP(int argc, char **argv){
 		pass = getpwuid(getuid()); 
 		sprintf(nom_rep, "/nfs/home/sasl/eleves/main/3520621/Documents/HPC/Path_tracing_HPC/%s", pass->pw_name);
 		mkdir(nom_rep, S_IRWXU);
-		sprintf(nom_sortie, "%s/version3_image%d.ppm", nom_rep,size);
+		sprintf(nom_sortie, "%s/version4_image%d.ppm", nom_rep,size);
 		
 		FILE *f = fopen(nom_sortie, "w");
 		fprintf(f, "P3\n%d %d\n%d\n", w, h, 255); 
