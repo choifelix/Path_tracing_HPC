e7ae43153f811296f4aed01fe9f93af68c2dc733
felix choi <felix.choi@free.fr>
2019-05-08 15:32:53 +0200

    test simd omp seq

--- Pathtracing_init.c
+++ Pathtracing_init.c
@@ -410,7 +410,7 @@ int main(int argc, char **argv)
 				for (int sub_j = 0; sub_j < 2; sub_j++) {
 					double subpixel_radiance[3] = {0, 0, 0};
 					/* simulation de monte-carlo : on effectue plein de lancers de rayons et on moyenne */
-					#pragma omp simd 
+					//#pragma omp simd 
 						for (int s = 0; s < samples; s++) { 
 							/* tire un rayon aléatoire dans une zone de la caméra qui correspond à peu près au pixel à calculer */
 							double r1 = 2 * erand48(PRNG_state);
