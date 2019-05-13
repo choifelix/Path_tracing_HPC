94f23106b80e51193444c1a38f025924b6896934
felix choi <felix.choi@free.fr>
2019-05-13 03:13:28 +0200

    test perfomance v3 gros cas

--- pathtracer_MPI.c
+++ pathtracer_MPI.c
@@ -3100,9 +3100,9 @@ int main(int argc, char **argv)
 	//version1_static(argc, argv);               // working fine 
 	//version2_beta_dynamic(argc, argv);         // not working
 	//version2_beta_dynamic_simple(argc, argv);  // working fine
-	//version3_dynamic_ring_token(argc, argv);   // working fine 
+	version3_dynamic_ring_token(argc, argv);   // working fine 
 	//version3_simd(argc, argv);				 // working fine
-	version4_openMP(argc, argv);				 // working fine
+	//version4_openMP(argc, argv);				 // working fine
 	
 	//version5_openMP_com(argc, argv);			 // not working 
 	
